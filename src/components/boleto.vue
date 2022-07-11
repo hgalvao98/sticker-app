1ba368e4f972dde5cabb03a8ab399986b2b9d3a4
hgalvao98 <henrique.galvao@hotmail.com>
2022-07-11 15:50:13 -0300

    created most of payment pages, missing adding form send functionalities and validation

--- src/components/boleto.vue
+++ src/components/boleto.vue
@@ -1,8 +1,45 @@
 <template>
-  <div class="a">a</div>
+  <div class="boleto">
+    <h1 class="boleto__title">Boleto gerado</h1>
+    <p class="boleto__instructions">
+      Você tem até 60 dias para pagar o boleto antes que ele vença
+    </p>
+    <div class="boleto__info">
+      <a class="boleto__copy" href="#">Copiar codigo</a>
+      <p class="boleto__code">
+        34191.79001 01043.510047 91020.150008 5 90430026000
+      </p>
+    </div>
+  </div>
 </template>
 <style>
-.a {
-  height: 100px;
+.boleto {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  border: 1px solid #2f3676;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.boleto__info {
+  margin: 0 auto;
+  text-align: center;
+  background-color: #2f3676;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+}
+
+.boleto__code {
+  margin-top: 10px;
+  border-top: 1px solid black;
+  padding-top: 10px;
+}
+
+a {
+  text-decoration: none;
+  color: white;
 }
 </style>
--- src/components/cartao.vue
+++ src/components/cartao.vue
@@ -1,3 +1,54 @@
+<script>
+const alert = () => {
+  alert("Card saved!");
+};
+</script>
+
 <template>
-  <div></div>
+  <div class="card">
+    <h1 class="card__title">Cartão de crédito</h1>
+    <form v-on:submit="alert">
+      <div class="form__field">
+        <label>Numero do cartão</label>
+        <input required />
+      </div>
+      <div class="form__field">
+        <label>Nome no cartão</label>
+        <input required type="text" />
+      </div>
+      <div class="form__field">
+        <label>Data de vencimento</label>
+        <input required type="date" />
+      </div>
+      <div class="form__field">
+        <label>CVV</label>
+        <input required type="password" maxlength="3" />
+      </div>
+      <button>submit</button>
+    </form>
+  </div>
 </template>
+
+<style>
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+  border: 1px solid #2f3676;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.form__field {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+}
+</style>
