2569a3c7d7b752d1b3fddf48e57de11f2eb3f5e0
hgalvao98 <henrique.galvao@hotmail.com>
2022-07-12 13:44:15 -0300

    organizing

---
+++ src/pages/MainQs.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Counter from "../components/counter.vue";
+</script>
+
+<template>
+  <form class="form" v-on:submit.prevent="onSubmit">
+    <div class="form__question">
+      <label class="form__question__title" for="">Quais adesivos:</label>
+      <div class="form__question__sticker">
+        <input type="checkbox" value="React" name="React" /><label>React</label>
+      </div>
+      <div class="form__question__sticker">
+        <input type="checkbox" value="Vue" name="Vue" /><label>Vue</label>
+      </div>
+      <div class="form__question__sticker">
+        <input type="checkbox" value="Angular" name="Angular" /><label
+          >Angular</label
+        >
+      </div>
+    </div>
+    <div class="form__question">
+      <Counter />
+    </div>
+    <div class="form__question">
+      <label class="form__question__title">Observações</label>
+      <textarea
+        class="form__textarea"
+        rows="8"
+        cols="50"
+        placeholder="Alguma dúvida? Recado?"
+      ></textarea>
+    </div>
+
+    <div class="form__send">
+      <button type="submit" class="form__send__button">Enviar</button>
+    </div>
+  </form>
+</template>
